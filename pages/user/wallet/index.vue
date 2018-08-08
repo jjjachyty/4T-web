9206d20f0d1d08e1878cb775c7cac6ca41dfff6a
zl <jjjachyty@163.com>
2018-08-08 22:25:36 +0800

    0808

--- pages/user/wallet/index.vue
+++ pages/user/wallet/index.vue
@@ -3,7 +3,6 @@
          <v-container>
              <v-layout justify-center>
                  <v-flex md6  xs12>
-  <v-form ref="form" v-model="valid" lazy-validation>
 
             <v-card>
                 <v-layout row wrap justify-center>
@@ -14,16 +13,40 @@
                     </v-flex>
                     <v-flex xs12 v-if="!showApplyCash">
                         <v-tabs grow>
-                            <v-tab>
+                            <v-tab href="#transactions">
                                 收入记录
                             </v-tab>
-                            <v-tab>
+                            <v-tab-item id="transactions">
+                                <div v-for="tran in transactions" :key="tran.id">
+                                    <v-card>
+                                        <v-card-title height="50"><span v-if="$store.state.user.id == tran.seller"><v-icon small>add</v-icon>收入</span><span v-else>支出</span><v-spacer></v-spacer><span class="red--text">¥{{tran.amount}}</span></v-card-title>
+                                        <v-divider></v-divider>
+                                        <v-card-text>
+                                            <v-layout wrap>
+                                                <v-flex xs12>订单号:<span class="grey--text">{{tran.orderID}}</span></v-flex>
+
+                                                <v-flex xs12>支付人:              <v-avatar size="20">
+                <img :src="avatarRoot+tran.buyer">
+              </v-avatar></v-flex>
+                                                              <v-flex xs12>支付时间:<span class="grey--text">{{tran.createAt|formatDate("YYYY-MM-DD HH:mm:ss")}}</span></v-flex>
+
+                                            </v-layout>
+                                        </v-card-text>
+                                    </v-card>
+                                    <br>
+                                </div>
+                            </v-tab-item>
+                            <v-tab href="#applycashs">
                                 提现记录
                             </v-tab>
+                            <v-tab-item id="applycashs">
+                                {{applyCashs}}
+                            </v-tab-item>
                         </v-tabs>
                     </v-flex>
                     <v-flex v-if="showApplyCash">
-                        <v-form>
+                          <v-form ref="form" v-model="valid" lazy-validation>
+
                             <v-layout wrap align-center justify-center>
 
                             <v-flex xs12 md5>
@@ -34,7 +57,7 @@
                                     <img v-if="bank.code" :src="'https://apimg.alipay.com/combo.png?d=cashier&t='+bank.code">
                                 </v-flex>
                             <v-flex xs12 md5>
-                            <v-text-field   prefix="¥" v-model.number="bank.amount" type="number" @blur="$v.bank.amount.$touch()" :error-messages="bankAmountErrors" label="提现金额" :placeholder="wallet.totalAmount+''"></v-text-field>
+                            <v-text-field   prefix="¥" v-model.number="amount" type="number" @blur="$v.amount.$touch()" :error-messages="bankAmountErrors" label="提现金额" :placeholder="wallet.totalAmount+''"></v-text-field>
                             </v-flex>
                             <v-flex xs12 md2 class="text-xs-center">
                                 <v-btn small outline color="primary" @click="applyCash">提现</v-btn>
@@ -44,7 +67,6 @@
                     </v-flex>
                 </v-layout>
             </v-card>
-  </v-form>
                     </v-flex>
              </v-layout>
         </v-container>
@@ -54,7 +76,7 @@
 </template>
 <script>
 import { validationMixin } from "vuelidate";
-import { required, minLength, maxLength, max } from "vuelidate/lib/validators";
+import { required, minLength, maxLength } from "vuelidate/lib/validators";
 export default {
   mixins: [validationMixin],
   validations: {
@@ -77,24 +99,27 @@ export default {
             return false;
           }
         }
-      },
-      amount: {
-        required,
-        validAmount(value) {
-          if (value > this.wallet.totalAmount) {
-            return false;
-          } else {
-            return true;
-          }
+      }
+    },
+    amount: {
+      required,
+      validAmount(value) {
+        if (value < this.wallet.totalAmount && value > 0) {
+          return true;
+        } else {
+          return false;
         }
       }
     }
   },
   data() {
     return {
-      valid: false,
+      valid: true,
+      amount: 0,
+      applyCashs: [],
+      transactions: [],
       bank: {
-        name: "",
+        code: "",
         number: ""
       },
       wallet: {
@@ -107,27 +132,28 @@ export default {
     bankCodeErrors() {
       const errors = [];
       if (!this.$v.bank.number.$dirty) return errors;
-      !this.$v.bank.number.validBankCode && errors.push("银行卡号识别错误");
       !this.$v.bank.number.minLength && errors.push("银行卡号为16-19位");
       !this.$v.bank.number.required && errors.push("银行卡号不能为空");
+      !this.$v.bank.number.validBankCode && errors.push("银行卡号识别错误");
+
       return errors;
     },
     bankAmountErrors() {
       const errors = [];
-      if (!this.$v.bank.amount.$dirty) return errors;
-      !this.$v.bank.amount.validAmount && errors.push("体现金额必须小于总金额");
-      !this.$v.bank.amount.required && errors.push("金额不能为空");
+      if (!this.$v.amount.$dirty) return errors;
+      !this.$v.amount.validAmount && errors.push("提现金额必须大于0小于总金额");
+      !this.$v.amount.required && errors.push("金额不能为空");
       return errors;
     }
   },
   methods: {
     applyCash() {
-      console.log("this.$refs.form.validate()", this.$refs.form.validate());
-      if (this.$refs.form.validate()) {
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
         this.$http
-          .post("/wallet/applycash", {
+          .postJson("/wallet/applycash", {
             amount: this.amount,
-            bankCard: this.bank
+            bankCard: { code: this.bank.code, number: this.bank.number }
           })
           .then(res => {
             if (res.data.Status) {
@@ -136,6 +162,19 @@ export default {
           });
       }
     }
+  },
+  mounted() {
+    this.$http.get("/wallet/applycashs", {}).then(res => {
+      if (res.data.Status) {
+        this.applyCashs = res.data.Data;
+      }
+    });
+
+    this.$http.get("/wallet/transactions", {}).then(res => {
+      if (res.data.Status) {
+        this.transactions = res.data.Data;
+      }
+    });
   }
 };
 </script>
